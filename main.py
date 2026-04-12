fe1815d0b4a28c3c7fe28f779c2660531df0f582
chiranjeev khanna <cherishkhanna10@gmail.com>
2026-04-12 22:28:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-Hotel_rooms={'A01':"Available",'A02':"Available",'A03':"Available",'A04':"Available",'A05':"Available",'B01':"Available",'B02':"Available",'B03':"Available",'B04':"Available",'B05':"Available",'C01':"Available",'C02':"Available",'C03':"Available",'C04':"Available",'C05':"Available"}
+Hotel_rooms={'A01':"Available",'A02':"Available",'A03':"Available",'A04':"Available",
+             'A05':"Available",'B01':"Available",'B02':"Available",'B03':"Available",
+             'B04':"Available",'B05':"Available",'C01':"Available",'C02':"Available",
+             'C03':"Available",'C04':"Available",'C05':"Available"}
 def welcome():
   print("Welcome to Cherish hotels")
 welcome()
@@ -7,9 +10,30 @@ def fuct():
   print("2, checkout room")
   print("3, show rooms")
 def book_room():
-  pass
-def check_room():
-  pass
+    found=False
+    for room in Hotel_rooms:
+        if Hotel_rooms[room]=="Available":
+          name=input("Enter the customer name ::")
+          phone=input("Enter the phone number of the customer ::")
+          aadhar=input("Enter the aadhar number of the customer ::")
+          Hotel_rooms[room]=[name,phone,aadhar]
+          print("Room",room,"has been booked successfully")
+          found=True
+          break
+    if found==False:
+        print("All rooms are booked")
+        
+def checkout_room():
+  room_num=input("Enter room number to be checkout ::")
+  if room_num in Hotel_rooms:
+      if Hotel_rooms[room_num]=="Available":
+          print("Room is already available")
+      else:
+          Hotel_rooms[room_num]="Available"
+          print(room_num,"is now available")
+          
+  else:
+      print("Wrong room number entered")
 def show_room():
   print("List of all rooms availability")
   for room in Hotel_rooms:
@@ -19,7 +43,7 @@ def show_room():
           print(room,"::","Booked")
 while True:
   fuct()
-  choice=input("Enter number alternative to the fuction to be performed(1(Book room)),(2(Checkout room)),(3(Show room)))
+  choice=input('''Enter number alternative to the fuction to be performed(1(Book room)),(2(Checkout room)),(3(Show room))''')
   if choice=="1":
     book_room()
   elif choice=="2":
@@ -34,6 +58,13 @@ while True:
   else:
     print("Thank you visit our hotel")
     break
+    
+  
+
+
+
+
+  
     
   
 
